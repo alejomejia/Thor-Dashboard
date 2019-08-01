b5fd0a08f32a8ad5919773716cfa7c207107e628
Alejandro Mejia <alejomejiacuartas@outlook.com>
2019-08-01 12:15:52 -0500

    Fix Client layout and delete some unnecesary styles

--- src/components/Client.js
+++ src/components/Client.js
@@ -3,23 +3,25 @@ import './styles/Client.sass'
 
 const Client = ({client, deleteClient}) => {
   return(
-    <div className="client">
-      <div className="card">
-        <div className="card-header">
-          {client.company}
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item"><i className="fa fa-user"></i> {client.name}</li>
-          <li className="list-group-item"><i className="fa fa-phone"></i> {client.phone}</li>
-          <li className="list-group-item"><i className="fa fa-envelope"></i> {client.email}</li>
-        </ul>
-        <div className="card-footer">
-          <button 
-            className="btn btn-danger"
-            onClick={() => deleteClient(client.id)}
-          >
-            Borrar cliente
-          </button>
+    <div className="col-md-4">
+      <div className="client">
+        <div className="card">
+          <div className="card-header">
+            {client.company}
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item"><i className="fa fa-user"></i> {client.name}</li>
+            <li className="list-group-item"><i className="fa fa-phone"></i> {client.phone}</li>
+            <li className="list-group-item"><i className="fa fa-envelope"></i> {client.email}</li>
+          </ul>
+          <div className="card-footer">
+            <button 
+              className="btn btn-danger"
+              onClick={() => deleteClient(client.id)}
+            >
+              Borrar cliente
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/ClientsList.js
+++ src/components/ClientsList.js
@@ -6,15 +6,13 @@ const ClientsList = ({clients, deleteClient}) => {
   return (
     <div className="clients-list">
       <div className="row">
-        <div className="col-md-4">
-          {clients.map( client => (
-            <Client 
-              key={client.id}
-              client={client} 
-              deleteClient={deleteClient}
-            />
-          ))}
-        </div>
+        {clients.map( client => (
+          <Client 
+            key={client.id}
+            client={client} 
+            deleteClient={deleteClient}
+          />
+        ))}
       </div>
     </div>
   );
