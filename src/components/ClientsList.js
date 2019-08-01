b2ec002f8cf1d7ba8c2479c26efa889758fe49a7
Alejandro Mejia <alejomejiacuartas@outlook.com>
2019-08-01 10:01:35 -0500

    Edit change from components to stateless functional

---
+++ src/components/ClientsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ClientsList extends Component {
+  render() {
+    return (
+      <div>
+        Cliente 1, 2, 3
+      </div>
+    );
+  }
+}
+
+export default ClientsList;
